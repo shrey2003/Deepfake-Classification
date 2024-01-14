ce688df6837fe3bbb123d15fd05e76955782a7a5
Shrey <shreymishra0307@gmail.com>
2024-01-14 17:45:30 +0530

    Added Complete Data Ingestion Pipeline

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from tumorclassification import logger
+from tumorclassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 logger.info("Welcome to Kidney-Tumor Classification Project")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> Starting {STAGE_NAME} <<<<<<")
+    obj= DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> Completed {STAGE_NAME} <<<<<<\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
